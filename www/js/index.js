f8ecfb67798ece4904c6b2f9fbaf86ab0ebc7ea6
ashutosh2k12 <216.ashutosh@gmail.com>
2014-06-17 04:33:24 +0530

    bbb

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,37 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+window.getLoginStatus =  function() {
+                FB.getLoginStatus(function(response) {
+                                  if (response.status == 'connected') {
+                                  alert('logged in');
+                                  } else {
+                                  alert('not logged in');
+                                  }
+                                  });
+}
+
+
+function logout() {
+                FB.logout(function(response) {
+                          alert('logged out');
+                          });
+}
+            
+function login() {
+	FB.login(
+			 function(response) {
+			 if (response.session) {
+			 alert('logged in');
+			 } else {
+			 alert('not logged in');
+			 }
+			 },
+			 { scope: "email" }
+			 );
+}
+			
 var app = {
     // Application Constructor
     initialize: function() {
@@ -34,7 +65,30 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		alert('Device is ready! Make sure you set your app_id below this alert.');
+		alert('Device is ready! Connecting to facebook...');
+		if (typeof CDV == 'undefined') alert('CDV variable does not exist. Check that you have included cdv-plugin-fb-connect.js correctly');
+        if (typeof FB == 'undefined') alert('FB variable does not exist. Check that you have included the Facebook JS SDK file.');
+		FB.Event.subscribe('auth.login', function(response) {
+                               alert('auth.login event');
+                               });
+            
+            FB.Event.subscribe('auth.logout', function(response) {
+                               alert('auth.logout event');
+                               });
+            
+            FB.Event.subscribe('auth.sessionChange', function(response) {
+                               alert('auth.sessionChange event');
+                               });
+            
+            FB.Event.subscribe('auth.statusChange', function(response) {
+                               alert('auth.statusChange event');
+                               });
+			try{
+				FB.init({ appId: "133722136790032", nativeInterface: CDV.FB, useCachedDialogs: false });
+				alert('connected');
+			}catch (e) {
+                                      alert(e);
+             }			
 		
     },
     // Update DOM on a Received Event
